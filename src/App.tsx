bc80a042668382d6b17f6eda65b46ed201ec0b37
Thibaud Lacan <tibo.lacan@gmail.com>
2023-03-21 10:37:45 +0100

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import Filters from "./components/Filters";
 import Hotels from "./components/Hotels";
-import { useHotels } from "./hooks/useHotels";
-import { useRooms } from "./hooks/useRooms";
 
 function App() {
   return (
