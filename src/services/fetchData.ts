d8ec16cfe240339be36e336e0ce2c80041784942
Thibaud Lacan <tibo.lacan@gmail.com>
2023-03-21 11:18:39 +0100

    Remove unused fucntions

--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -1,17 +1,3 @@
 import axios from "axios";
 
-export const fetchHotels = async () => {
-  const response = await fetch(
-    "https://obmng.dbm.guestline.net/api/hotels?collection-id=OBMNG"
-  );
-  return response;
-};
-
-export const fetchRooms = async (hotelId: string) => {
-  const response = await fetch(
-    `https://obmng.dbm.guestline.net/api/roomRates/OBMNG/${hotelId}/`
-  );
-  return response;
-};
-
 export const fetcher = (url: string) => axios.get(url).then((res) => res.data);
